718021999df0fe3e4f21bc96c61cfd3ff289ee84
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-02 10:22:43 -0300

    funciones de respond comunes

---
+++ utils/respond.go
@@ -0,0 +1,24 @@
+package respond
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+func respondJSON(w http.ResponseWriter, status int, results interface{}) {
+
+	response, err := json.Marshal(results)
+	if err != nil {
+		w.WriteHeader(http.StatusInternalServerError)
+		w.Write([]byte(err.Error()))
+		return
+	}
+	w.Header().Set("Content-Type", "application/json")
+	w.WriteHeader(status)
+	w.Write([]byte(response))
+
+}
+
+func respondError(w http.ResponseWriter, code int, message string) {
+	respondJSON(w, code, map[string]string{"error": message})
+}
