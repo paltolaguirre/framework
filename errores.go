8ede3f28a22f0418b60d629c5e09953f3e298cd3
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-08 12:12:49 -0300

    nuevos errores

--- errores.go
+++ errores.go
@@ -2,4 +2,6 @@ package framework
 
 const (
 	IdParametroDistintoStruct = "El ID de la url debe ser el mismo que el del struct"
+	IdParametroVacio          = "Debe ingresar un ID en la url"
+	DatosIncorrectos          = "Los datos ingresados son incorrectos"
 )
