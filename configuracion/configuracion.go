195e1185e691e381daa19cea5d83db299d5edcce
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-09 13:57:11 -0300

    prueba para saber en que carpeta estan los ejecutables

--- configuracion/configuracion.go
+++ configuracion/configuracion.go
@@ -3,6 +3,7 @@ package configuracion
 import (
 	"fmt"
 	"os"
+	"path/filepath"
 )
 
 type Configuracion struct {
@@ -17,10 +18,15 @@ func GetInstance() Configuracion {
 	instance = Configuracion{}
 
 	path, err := os.Executable()
+
 	if err != nil {
 		fmt.Println(err)
 	}
+	dir := filepath.Dir(path)
+
 	fmt.Println(path)
+	fmt.Println("dir")
+	fmt.Println(dir)
 	instance.Ip = "192.168.30.111"
 	instance.Namedb = "faf_multitenant_go"
 	return instance
