12821b1842769820d3c8883057a9c7bb19323524
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-15 13:06:44 -0300

    prueba para el metodo list

--- actions.go
+++ actions.go
@@ -1,81 +1,13 @@
 package framework
 
 import (
-	"encoding/json"
-	"fmt"
-	"io/ioutil"
 	"net/http"
 
 	"github.com/jinzhu/gorm"
-	"github.com/xubiosueldos/autenticacion/publico"
-	"github.com/xubiosueldos/conexionBD"
 )
 
-func ServicioList(w http.ResponseWriter, r *http.Request, servicio struct{}, db *gorm.DB) {
+func ServicioList(w http.ResponseWriter, r *http.Request, servicio interface{}, db *gorm.DB) {
 
-	tokenAutenticacion, tokenError := checkTokenValido(r)
-	fmt.Println(tokenAutenticacion)
-	if tokenError != nil {
-		errorToken(w, tokenError)
-		return
-	} else {
-
-		//db := obtenerDB(tokenAutenticacion)
-
-		//defer db.Close()
-
-		//Lista todos los legajos
-		db.Find(&servicio)
-
-		RespondJSON(w, http.StatusOK, servicio)
-	}
-
-}
-
-func checkTokenValido(r *http.Request) (*publico.Security, *publico.Error) {
-
-	var tokenAutenticacion *publico.Security
-	var tokenError *publico.Error
-
-	url := "http://localhost:8081/check-token"
-
-	req, _ := http.NewRequest("GET", url, nil)
-
-	header := r.Header.Get("Authorization")
-
-	req.Header.Add("Authorization", header)
-
-	res, _ := http.DefaultClient.Do(req)
-
-	defer res.Body.Close()
-	body, _ := ioutil.ReadAll(res.Body)
-
-	if res.StatusCode != http.StatusBadRequest {
-
-		// tokenAutenticacion = &(TokenAutenticacion{})
-		tokenAutenticacion = new(publico.Security)
-		json.Unmarshal([]byte(string(body)), tokenAutenticacion)
-
-	} else {
-		tokenError = new(publico.Error)
-		json.Unmarshal([]byte(string(body)), tokenError)
-
-	}
-
-	return tokenAutenticacion, tokenError
-}
-
-func errorToken(w http.ResponseWriter, tokenError *publico.Error) {
-	errorToken := *tokenError
-	RespondError(w, errorToken.ErrorCodigo, errorToken.ErrorNombre)
-
-}
-
-func obtenerDB(tokenAutenticacion *publico.Security) *gorm.DB {
-
-	token := *tokenAutenticacion
-	tenant := token.Tenant
-
-	return conexionBD.ConnectBD(tenant)
+	db.Find(&servicio)
 
 }
