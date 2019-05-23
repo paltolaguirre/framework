5bbfef1620856de3143d5fb1737b8e744db244ae
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-05-23 10:10:15 -0300

    nuevas constantes para el manejo de errores

--- errores.go
+++ errores.go
@@ -4,6 +4,9 @@ const (
 	IdParametroDistintoStruct = "El ID de la url debe ser el mismo que el del struct"
 	IdParametroVacio          = "Debe ingresar un ID en la url"
 	DatosIncorrectos          = "Los datos ingresados son incorrectos"
-	LegajoEliminado           = "Se ha eliminado correctamente el legajo con ID: "
-	NovedadEliminada          = "Se ha eliminado correctamente la novedad con ID: "
+	Legajo                    = "El legajo con ID: "
+	Novedad                   = "La novedad con ID: "
+	Liquidacion               = "La liquidación con ID: "
+	Concepto                  = "El concepto con ID: "
+	MicroservicioEliminado    = "Se ha eliminado correctamente."
 )
