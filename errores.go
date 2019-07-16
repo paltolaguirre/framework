07e9d38331c0b0c122654700a549298039056742
Paula Altolaguirre <paltolaguirre@finnegans.com.ar>
2019-07-16 14:43:43 -0300

    Nuevos mensajes de error para liquidaciones contabilizadas

--- errores.go
+++ errores.go
@@ -1,12 +1,17 @@
 package framework
 
 const (
-	IdParametroDistintoStruct = "El ID de la url debe ser el mismo que el del struct"
-	IdParametroVacio          = "Debe ingresar un ID en la url"
-	DatosIncorrectos          = "Los datos ingresados son incorrectos"
-	Legajo                    = "El legajo con ID: "
-	Novedad                   = "La novedad con ID: "
-	Liquidacion               = "La liquidación con ID: "
-	Concepto                  = "El concepto con ID: "
-	MicroservicioEliminado    = " se ha eliminado correctamente."
+	IdParametroDistintoStruct                                   = "El ID de la url debe ser el mismo que el del struct"
+	IdParametroVacio                                            = "Debe ingresar un ID en la url"
+	DatosIncorrectos                                            = "Los datos ingresados son incorrectos"
+	Legajo                                                      = "El legajo con ID: "
+	Novedad                                                     = "La novedad con ID: "
+	Liquidacion                                                 = "La liquidación con ID: "
+	Concepto                                                    = "El concepto con ID: "
+	MicroservicioEliminado                                      = " se ha eliminado correctamente."
+	Seleccioneunregistro                                        = "Seleccione por lo menos un registro."
+	Descripcionasientomanualcontableliquidacionescontabilizadas = "Contabilización automática de las Liquidaciones de Sueldos"
+	Seleccionaronliquidacionescontabilizadas                    = "Se seleccionaron liquidaciones ya contabilizadas"
+	Modificarliquidacioncontabilizada                           = "La liquidación que se quiere modificar se encuentra contabilizada"
+	Eliminarliquidacioncontabilizada                            = "La liquidación que se quiere eliminar se encuentra contabilizada"
 )
