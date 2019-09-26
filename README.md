8cef5c08f71386d95cad42414a5751a427a56aab
Carlos <cflores@xubio.com>
2019-09-26 12:15:30 -0300

    Primer Unit Test con Go
    UTest para configuracion.go

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # framework
+
+## Unit Test
+
+### Correr todos los tests
+```go test ./... -coverprofile=cover.out```
+
+### Correr todos los tests y ver detalles
+```go test ./... -coverprofile=cover.out && go tool cover -html=cover.out```
---
+++ configuracion/configuracion_test.go
@@ -0,0 +1,45 @@
+package configuracion
+
+import "testing"
+
+func TestGetConfig(t *testing.T) {
+	t.Run("Check Ip", func(t *testing.T) {
+		expected := "192.168.30.116"
+		got := GetInstance()
+
+		if expected != got.Ip {
+			t.Errorf("got '%s' expected '%s'", got.Ip, expected)
+		}
+	})
+
+	t.Run("Check Namedb", func(t *testing.T) {
+		expected := "FAF_MULTITENANT_GO"
+		got := GetInstance()
+
+		if expected != got.Namedb {
+			t.Errorf("got '%s' expected '%s'", got.Namedb, expected)
+		}
+	})
+}
+
+func TestGetUrlMonolitico(t *testing.T) {
+	t.Run("Check GetUrlMonolitico", func(t *testing.T) {
+		expected := "https://192.168.30.112:8443/NXV/"
+		got := GetUrlMonolitico()
+
+		if expected != got {
+			t.Errorf("got '%s' expected '%s'", got, expected)
+		}
+	})
+}
+
+func TestGetUrlMicroservicio(t *testing.T) {
+	t.Run("Check GetUrlMicroservicio", func(t *testing.T) {
+		expected := "http://localhost:80/api/"
+		got := GetUrlMicroservicio("80")
+
+		if expected != got {
+			t.Errorf("got '%s' expected '%s'", got, expected)
+		}
+	})
+}
